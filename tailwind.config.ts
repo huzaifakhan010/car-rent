432351186352f5ef22a614c4a0241864c010e064
huzaifa <164413458+m5huzefa@users.noreply.github.com>
2025-01-08 00:54:09 +0500

    3nd commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,16 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages//*.{js,ts,jsx,tsx,mdx}",
+    "./src/components//*.{js,ts,jsx,tsx,mdx}",
+    "./src/app//*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "var(--background)", // Use your CSS variable for the background
+        foreground: "var(--foreground)", // Use your CSS variable for the foreground
+        border: "var(--border)", // Define a CSS variable for the border
       },
     },
   },
